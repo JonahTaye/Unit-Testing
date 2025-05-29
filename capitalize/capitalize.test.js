bfe1aafd4bb1e4626e76bc7c0a2d8ffd6b65dd70
jonah <jonah.softwaredev@gmail.com>
2025-05-29 12:26:25 +0300

    test: add unit tests for capitalize module

---
+++ capitalize/capitalize.test.js
@@ -0,0 +1,13 @@
+import capitalize from "./capitalize.js"
+
+describe("Capitalize first letter of a string", () => {
+    test("Capitalizes a word", () => {
+        expect(capitalize("hello")).toEqual("Hello")
+    })
+    test("Capitalizes a word - regex test", () => {
+        expect(capitalize("hello")).toMatch(/^[A-Z].+/)
+    })
+    test("Capitalizes a sentence", () => {
+        expect(capitalize("hello world")).toEqual("Hello World")
+    })
+})
