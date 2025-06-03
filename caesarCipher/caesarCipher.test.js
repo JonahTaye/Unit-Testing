69bcbe8c86c25a98ad134ccfab7db948ba5776ab
jonah <jonah.softwaredev@gmail.com>
2025-06-03 10:13:44 +0300

    test: add test cases

---
+++ caesarCipher/caesarCipher.test.js
@@ -0,0 +1,14 @@
+describe("Caesar Cipher Function Test", () => {
+    test("abc to def", () => {
+        expect(caesarCipher("abc", 3)).toEqual("def")
+    })
+    test("xyz to abc", () => {
+        expect(caesarCipher("xyz", 3)).toEqual("abc")
+    })
+    test("Capital letter test", () => {
+        expect(caesarCipher("Hello", 3)).toEqual("KhOOr")
+    })
+    test("Punctuation mark test", () => {
+        expect(caesarCipher("Hello, World!", 3)).toEqual("Khoor, Zruog!")
+    })
+})
